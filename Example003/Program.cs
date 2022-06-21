06fec0d8e6405e4546e6ea52042a17b8ae29c052
Denis <9581886@gmail.com>
2022-06-21 21:21:34 +0300

    first commit

---
+++ Example003/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите первое число: ");
+int FirstNumber = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Первое число: " + FirstNumber);
+Console.Write("Введите второе число: ");
+int SecondNumber = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Второе число: " + SecondNumber);
+
+Console.WriteLine("Сумма двух чисел: " + (FirstNumber+SecondNumber));
