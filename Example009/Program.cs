d2cd45e9558e05ebca5ff6c5e818e2d8ffad00b3
Denis <9581886@gmail.com>
2022-06-26 22:47:15 +0300

    first commit

---
+++ Example009/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {11, 21, 35, 9, 51, 64, 11, 5, 9};
+
+int n = array.Length;
+int index = 0;
+int find=9;
+
+while (index<n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+index++;
+}
